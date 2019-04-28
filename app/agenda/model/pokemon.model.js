c661924ec49e55e6cf5bcd52a5a2e9872099ccee
lesimoes <lesimoes0@gmail.com>
2019-04-28 13:56:50 -0300

    Fix

--- app/agenda/model/pokemon.model.js
+++ app/agenda/model/pokemon.model.js
@@ -1,18 +1,21 @@
+// Classe de Pokemon
+
 export default class Pokemon {
 
+  //Construtor da classe
   constructor(name, type){
     this.name = name
     this.type = type
   }
-
+  //Inseri um nome para o Pokemon
   setName(name){
     this.name = name
   }
-
+//Retorna o nome do Pokemon
   getName(){
     return this.name;
   }
-
+//Retornar o tipo de Pokemon
   getType(){
     return this.type
   }
